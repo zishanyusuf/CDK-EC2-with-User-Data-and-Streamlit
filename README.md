325f996d6ab06db63b2f809bffc50ca81055cf96
Zishan <zishan_yusuf@yahoo.co.in>
2025-01-13 19:49:14 +0100

    EC2 with User Data final

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
+# How to create EC2 with User Data using CDK Python pre-configured with Streamlit App file
+This project showcases how to create an EC2 instance with CDK Python with the addtion of the `User Data` script. User Data script helps pre-install all the required updates and helps create a ready to consume platform to hit the ground running. 
+
+In this example, Users sets up an EC2 with CDK that prepares to install and run streamlit app on EC2
+
+**1. User deploys this CDK to create EC2. The EC2 is created with following configurations**
+1.1. EC2 creation with appropriate Security groups, Pairing Keys
+1.2. HTTP Outbound rules that allows Streamlit to host
+1.3. Creates HTTP with port required for the Streamlit
+1.4. Contains the `User Data` that installs the latest updates
+1.5. `User Data` also copies a sample streamlit app in the path /home/ec2-user/streamlit-app. The file app.py contains the sample streamlit app that users can modify to suite her purpose  
+
+**2. As a one time activity, User then installs Streamlit manually accessing the EC2. Here are the manual steps listed**
+2.1. Access the EC2 with Online Navigate on AWS Console>EC2>Instances>Select `/home/ec2-user/streamlit-app`. Then click Connect for online access of the EC2
+2.2. Install the streamlit type `pip3 install streamlit`
+
+**3. Launch the Streamlit App for Temporary Testing**
+3.1. Launch the streamlit app.py by typing following command `streamlit run /home/ec2-user/streamlit-app/app.py`
+3.2. Copy paste the External URL generated in Web Browser to access the Streamlit App 
+
+**4. Launch the Streamlit App for Permanent**
+4.1. Launch the streamlit app.py by typing following command `nohup python3 -m streamlit run /home/ec2-user/streamlit-app/app.py`
+4.2. Copy paste the External URL generated in Web Browser to access the Streamlit App
+
+**5. For any enhancemnet in the streamlit app, the users can modify the the files app.py `home/ec2-user/streamlit-app/app.py`**
+
+
 
 # Welcome to your CDK Python project!
 
--- cdk_streamlit_ec2/cdk_streamlit_ec2_stack.py
+++ cdk_streamlit_ec2/cdk_streamlit_ec2_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    CfnOutput
 )
 from constructs import Construct
 
@@ -10,10 +11,76 @@ class CdkStreamlitEc2Stack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        #Create the EC2, it contains the following steps
+        #Step 1. Create VPC, pre-requiste to create the EC2
+        vpc = ec2.Vpc(
+            self,
+            "StreamlitEc2Vpc", #Mandatory field
+            max_azs=2, #Optional - Defines the max AZ
+            subnet_configuration=[ #Optional - Defines the subnet configuration. by default it creates the Public and Private both and distribute the CIDER equally. But here we force just to create the public only
+                ec2.SubnetConfiguration(
+                    name="public-subnet-1",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24,
+                )
+            ],
+        )
+        
+        #Step 2. Create Security Group, pre-requiste to create the EC2
+        sec_group = ec2.SecurityGroup(
+            self, "StreamlitEc2SecurityGroup",
+            vpc=vpc,
+            allow_all_outbound=True,
+        )
+        
+        #Add inbound rules in the Above Security group to allow HTTP, HTTPS traffic + Streamlit port traffic 
+        #Step 2.1 Add inbound rule for HTTP (port 80)
+        sec_group.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(80),
+            description="Allow HTTP traffic"
+        )
+        #Step 2.2 Add inbound rule for HTTPS (port 443)
+        sec_group.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(443),
+            description="Allow HTTPS traffic"
+        )
+        #Step 2.3 Add inbound rule for Streamlit (port 8501)
+        sec_group.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(8501),
+            description="Allow Streamlit traffic"
+        )
+        #Step 2.4 Add inbound rule for SSH (port 22). This will help launch EC2 CLI on the web console
+        sec_group.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(22),
+            description="Allow SSH traffic"
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkStreamlitEc2Queue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        #Step 3. Create Key Pair, pre-requiste to create the EC2
+        cfn_key_pair = ec2.CfnKeyPair(
+            self, "StreamlitEc2KeyPair",
+            key_name="streamlit-key-pair",
+        )
+                
+        #Step 4. Finally create the EC2 in last steps
+        instance = ec2.Instance(
+            self,
+            "StreamlitInstance",
+            instance_type=ec2.InstanceType("t2.small"),
+            machine_image=ec2.MachineImage.latest_amazon_linux2023(),
+            vpc=vpc,
+            security_group=sec_group,
+            associate_public_ip_address=True,
+            key_name=cfn_key_pair.key_name
+        )
+        
+        #Add User Data to the EC2
+        with open("./user_data.sh", "r") as file:
+            user_data_script = file.read()
+        instance.add_user_data(user_data_script)
+        
+        # Output Instance ID
+        CfnOutput(self, "InstanceId", value=instance.instance_id)
