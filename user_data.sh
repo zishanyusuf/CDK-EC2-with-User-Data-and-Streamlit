52bd6174ba075527e1061e49c0af05b84674baba
Zishan <zishan_yusuf@yahoo.co.in>
2025-01-14 12:47:01 +0100

    streamlit installed but running error

--- user_data.sh
+++ user_data.sh
@@ -1,13 +1,13 @@
 #! /bin/bash -x
 sudo yum update
 sudo yum upgrade -y
-sudo yum install python3-pip -y
+sudo yum install python3-pip3 -y
 #Install the Git packages
 sudo yum install git -y
 #Clone the Git page containing the streamlit app
 sudo mkdir -p /home/ec2-user/streamlit-app
 cd /home/ec2-user/streamlit-app
 sudo git clone https://github.com/zishanyusuf/Sample-Streamlit-App /home/ec2-user/streamlit-app
-
+pip3 install -r /home/ec2-user/streamlit-app/requirements.txt
 ##Print the "User data script executed" into a log file (named user-data.log) within a path /var/log
 echo 'User data script executed' > /var/log/user-data.log
